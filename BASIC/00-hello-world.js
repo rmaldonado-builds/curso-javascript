0194159e1c0672524055f8c1e79c2709b7102774
rmaldonado-builds <ronaldmartinmaldonado@gmail.com>
2025-10-23 12:30:42 -0500

    Agrego archivo 00-hello-world.js

--- BASIC/00-hello-world.js
+++ BASIC/00-hello-world.js
@@ -1,3 +1,18 @@
+// Esto es un comentario de prueba
+
 console.log("Hola, JavaScript!");
 console.log('Hola, JavaScript!');
 console.log(`Hola, JavaScript!`);
+
+/*
+  Este es un comentario
+  de múltiples líneas
+*/
+
+/* Una prueba de como usar los comentarios para que no se ejecute el siguiente código
+console.log("Hola, JavaScript!");
+console.log('Hola, JavaScript!');
+console.log(`Hola, JavaScript!`);
+*/
+
+console.log("5");
