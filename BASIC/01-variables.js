966aec980aeae1ce602c912de7ad2702f52ce8de
rmaldonado-builds <ronaldmartinmaldonado@gmail.com>
2025-10-23 19:33:20 -0500

    guardado progreso 01 variables.js

--- BASIC/01-variables.js
+++ BASIC/01-variables.js
@@ -5,18 +5,21 @@ console.log(helloWorld); // sin existir la variable, devuelve 'undefined'
 var helloWorld = "¡Hola, JavaScript con var!";
 console.log(helloWorld);
 
-helloWorld = "¡Hola de nuevo, JavaScript con var!";
+helloWorld = "¡Hola de nuevo, JavaScript con var, modificada la variable!";
 console.log(helloWorld);
 
-// let
+// let *** al crear la variable no se eleva al inicio del fichero
 
 let helloWorld2 = "¡Hola, JavaScript2 con let!";
 console.log(helloWorld2);
 
-helloWorld2 = "¡Hola de nuevo, JavaScript2 con let!";
+helloWorld2 = "¡Hola de nuevo, JavaScript2 con let, modificada la variable!";
 console.log(helloWorld2);
     
-// const
+// const *** al crear la variable no permite reasignación
 
 const helloWorld3 = "¡Hola, JavaScript3 con const!";
 console.log(helloWorld3);
+
+// helloWorld3 = "¡Hola, JavaScript3 con const!"; *** no permite reasignación, lanzará un error
+
