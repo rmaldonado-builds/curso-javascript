90f45b33e449369446f78964caf889e1186c8c4a
rmaldonado-builds <ronaldmartinmaldonado@gmail.com>
2025-10-26 21:31:24 -0500

    guardo progreso ejercicios

---
+++ BASIC/03-beginner-exercices.js
@@ -0,0 +1,40 @@
+// Ejercicios: primeros pasos
+
+// 1. Escribe un comentario en una línea
+// Este es un comentario en una linea
+
+// 2. Escribe un comentario en varias líneas
+/* este es un comentario 
+en varias lineas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let variablestring = "Esto es una cadena de texto"
+let variablenumber = 85
+let variableboolean = true
+let variableundefined = undefined
+let variablenull = null
+let variablesymbol = Symbol("valor unico")
+let variableBigInt = 123456789012345678901234567890n
+let variableBigInt2 = BigInt("12345678912345678901234567890")
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(variablestring);
+console.log(variablenumber);
+console.log(variableboolean);
+console.log(variableundefined);
+console.log(variablenull);
+console.log(variablesymbol);
+console.log(variableBigInt);
+console.log(variableBigInt2);
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
